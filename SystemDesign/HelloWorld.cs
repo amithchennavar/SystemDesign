3e7d9cafedd294fad2b6ba6c63daf8262425b1a0
amithchennavar <amcr18ec@cmrit.ac.in>
2024-04-28 21:59:35 +0530

    Add project files.

---
+++ SystemDesign/HelloWorld.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SystemDesign
+{
+    public class HelloWorld
+    {
+        public void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+        }
+
+    }
+}
